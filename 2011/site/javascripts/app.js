eecdf5533d18c7236cdfc0587f81e3be8a50779c
Chris Williams <chris@iterativedesigns.com>
2011-06-03 11:49:44 -0400

    Adding OPower and Phono as sponsors.

--- 2011/site/javascripts/app.js
+++ 2011/site/javascripts/app.js
@@ -1,6 +1,12 @@
+
+
 dojo.addOnLoad( function() { 
-
-
+  var ads = dojo.query("#right .sponsor");
+  if (ads.length > 0) {
+    ads.orphan();  // remove from dom
+    ads.sort(function () {  return (Math.round(Math.random())-0.5); }); // randomize
+    dojo.forEach(ads, "dojo.place(item,'right');")
+  }
   var d = new Date();
   var txt = "day"
   if (d.getHours() <= 4 || d.getHours() > 17 ) {
@@ -11,6 +17,8 @@ dojo.addOnLoad( function() {
     txt = "afternoon"
   } 
   dojo.byId("time").innerHTML = txt;
+  
+  
   
   dojo.query('.sponsor').onmouseover(function(evt) {
     dojo.addClass(evt.currentTarget, 'hover');
